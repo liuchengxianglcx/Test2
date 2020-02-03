65246f3f465ab36400c5b54c005619cb336224ed
liuchengxianglcx <1016852263@qq.com>
2020-02-03 23:04:49 +0800

    test2

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('你已成功分享了该产品!');
+  }
+
+  onClick() {
+    window.alert("已通知！")
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
