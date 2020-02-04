5b6037d2c0680f21648779de1d32f6d93384e268
liuchengxianglcx <1016852263@qq.com>
2020-02-04 22:14:53 +0800

    test2

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
+import {CartService} from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -10,12 +11,19 @@ import { products } from '../products';
 export class ProductDetailsComponent implements OnInit {
 
   product;
-  constructor(private route: ActivatedRoute,) { }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.product = products[+params.get('productId')];
-    });
+    });  
   }
+
+  addToCart(product) {
+      window.alert("你已成功添加了该商品到购物车中");
+      this.cartService.addToCart(product);
+    }
 
 }
